dbdd6ff328bb4d54eb50518b081c83ad7d89fdfb
Mária Aksamít <maria.aksamitova@gmail.com>
2024-05-04 12:47:02 +0200

    mock

--- client/src/ListProvider.js
+++ client/src/ListProvider.js
@@ -1,11 +1,13 @@
 import React, { createContext, useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { toggleMock } from './calls';
+import { mockedLists } from './mockedLists';
 
 const ListContext = createContext();
 
 export function ListProvider({ children })  {
+  const useMock = toggleMock();
   const { t } = useTranslation();
-
   const [lists, setLists] = useState([]);
   const [status, setStatus] = useState({
     state: "pending",
@@ -38,13 +40,22 @@ export function ListProvider({ children })  {
   const fetchList = async () => {
     setStatus({ state: "pending" });
     try {
-      const response = await fetch('http://127.0.0.1:8000/shoppingList/list');
-      if (!response.ok) {
-        throw new Error(t('Error loading data'));
+      let url;
+      if (useMock) {
+        // MOCK
+        setLists(mockedLists); 
+        setStatus({ state: "success" });
+      } else {
+        // SERVER
+        url = 'http://127.0.0.1:8000/shoppingList/list';
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(t('Error loading data'));
+        }
+        const data = await response.json();
+        setLists(data);
+        setStatus({ state: "success" });
       }
-      const data = await response.json();
-      setLists(data);
-      setStatus({ state: "success" });
     } catch (error) {
       console.error(t('Error loading data:'), error);
       setStatus({ state: "error", error });
@@ -52,21 +63,24 @@ export function ListProvider({ children })  {
   };
 
   const createList = async (newList, callback) => {
-    await serverCall('http://127.0.0.1:8000/shoppingList/create', newList, 'POST', error => {
+    const url = useMock ? '/mockedCreateList' : 'http://127.0.0.1:8000/shoppingList/create';
+    await serverCall(url, newList, 'POST', error => {
       console.error('Error creating list:', error);
     });
     callback && callback();
   };
 
   const updateList = async (updatedList, callback) => {
-    await serverCall('http://127.0.0.1:8000/shoppingList/update', updatedList, 'POST', error => {
+    const url = useMock ? '/mockedCreateList' : 'http://127.0.0.1:8000/shoppingList/update';
+    await serverCall(url, updatedList, 'POST', error => {
       console.error('Error updating list:', error);
     });
     callback && callback();
   };
 
   const deleteList = async (requestData, callback) => {
-    await serverCall('http://127.0.0.1:8000/shoppingList/delete', requestData, 'POST', error => {
+    const url = useMock ? '/mockedCreateList' : 'http://127.0.0.1:8000/shoppingList/delete';
+    await serverCall(url, requestData, 'POST', error => {
       console.error('Error deleting list:', error);
     });
     callback && callback();
--- client/src/UserProvider.js
+++ client/src/UserProvider.js
@@ -1,4 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
+import { toggleMock } from './calls';
+
 
 const UserContext = createContext();
 
@@ -9,6 +11,8 @@ export function UserProvider({ children })  {
  const [user, setUser] = useState(null);
 
  useEffect(() => {
+  if (!toggleMock()) {
+    // Server
     fetch('http://127.0.0.1:8000/users')
        .then(response => response.json())
        .then(data => {
@@ -18,7 +22,14 @@ export function UserProvider({ children })  {
          setIsLoggedIn(!!alreadyLogged);
        })
        .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  } else {
+    // Mock
+    setUsers(mockedUsers); 
+    const alreadyLogged = JSON.parse(sessionStorage.getItem('authUser'));
+    setUser(alreadyLogged ?? mockedUsers.find(user => user.id === 0)); 
+    setIsLoggedIn(!!alreadyLogged);
+  }
+}, []);
 
   const changeUser = (userId) => {
     const selectedUser = users.find(user => user.id === userId);
@@ -59,5 +70,68 @@ return (
     </UserContext.Provider>
   );
 };
+
+const mockedUsers = [
+  {
+    "id": "012f5e1c",
+    "name": "Tessa Gray"
+  },
+  {
+    "id": "02743d9e",
+    "name": "William Herondale"
+  },
+  {
+    "id": "03b0e8d7",
+    "name": "Charlotte Branwell"
+  },
+  {
+    "id": "041a3c9b",
+    "name": "Jessamine Lovelace"
+  },
+  {
+    "id": "05f7c4a9",
+    "name": "Emma Carstairs"
+  },
+  {
+    "id": "069d7e0f",
+    "name": "Cassandra Clare"
+  },
+  {
+    "id": "07c6d1e2",
+    "name": "Jace Wayland"
+  },
+  {
+    "id": "082b6d4a",
+    "name": "Jocelyn Morgernstern"
+  },
+  {
+    "id": "099e3a8b",
+    "name": "Alec Lightwood"
+  },
+  {
+    "id": "10e6f8c7",
+    "name": "Clary Fairchild"
+  },
+  {
+    "id": "114b1d9f",
+    "name": "Hodge Starkweather"
+  },
+  {
+    "id": "122f8c7a",
+    "name": "Simon Lewis"
+  },
+  {
+    "id": "13d4a7e5",
+    "name": "Magnus Bane"
+  },
+  {
+    "id": "146f2e8b",
+    "name": "Isabelle Lightwood"
+  },
+  {
+    "id": "155d7f9e",
+    "name": "Aline Penhallow"
+  }
+]
 
 export default UserContext;
